7662ed8bd86132ea66905a90590905d907ad2120
MEENAN-721 <mn0422790@gmail.com>
2025-11-16 16:41:07 +0000

    First

---
+++ Corejavaprogram/src/inheritancedemo/Father.java
@@ -0,0 +1,18 @@
+package inheritancedemo;
+
+public class Father {
+	// properties
+		int money=50000;
+		String car="BMW";
+		
+		// behaviour (method)
+		
+		void drinking() {
+			System.out.println("father likes to drink tea");
+		}
+		
+		void read() {
+		System.out.println("news paper");	
+		}
+
+}
