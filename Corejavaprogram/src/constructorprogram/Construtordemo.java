ce1f7258bfdfcf9742db2fe890757ad2b04d41b7
MEENAN-721 <mn0422790@gmail.com>
2025-12-14 14:21:35 +0000

    First

--- Corejavaprogram/src/constructorprogram/Construtordemo.java
+++ Corejavaprogram/src/constructorprogram/Construtordemo.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Construtordemo {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		
 		System.out.println("enter the name");
 		String name=sc.next();
 		System.out.println("enter the address");
