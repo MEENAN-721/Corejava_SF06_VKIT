0564b2b3bd8542d9abe57749543a75afcdde3994
MEENAN-721 <mn0422790@gmail.com>
2025-12-14 16:14:02 +0000

    First

---
+++ Corejavaprogram/src/com/tnsif/staticprogram/Staticblockdemo.java
@@ -0,0 +1,11 @@
+package com.tnsif.staticprogram;
+
+public class Staticblockdemo {
+	static {
+		System.out.println("welcome to vkit");
+	}
+	public static void main(String[] args) {
+		System.out.println("welcome to java");
+	}
+
+}
